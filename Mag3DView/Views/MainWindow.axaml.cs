22d0b4596cc9a680b7f4c099370f78012d6172c8
Paarsah <paarsah.soroury@gmail.com>
2024-09-19 18:24:48 -0700

    Create a new project: Mag3DView

---
+++ Mag3DView/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Mag3DView.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
